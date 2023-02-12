6eb1bd7c226f86b7bcbd7f159a29683f793617a5
Garry <garrysjh@gmail.com>
2023-02-12 22:29:48 +0800

    fixed bug

--- src/main/java/com/springtest/api/listings/ListingController.java
+++ src/main/java/com/springtest/api/listings/ListingController.java
@@ -2,6 +2,8 @@ package com.springtest.api.listings;
 
 
 import com.springtest.models.Listing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/listings")
 public class ListingController {
-
+@Autowired
     private final ListingService listingService;
     @GetMapping("/listings")
     public List<Listing> fetchAllListings(){
+        return listingService.getAllListings();
+    }
+    @GetMapping("/test")
+    public List<Listing> index() {
         return listingService.getAllListings();
     }
 
--- src/main/java/com/springtest/api/listings/ListingService.java
+++ src/main/java/com/springtest/api/listings/ListingService.java
@@ -1,12 +1,13 @@
 package com.springtest.api.listings;
 
 import com.springtest.models.Listing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class ListingService {
-
+@Autowired
     private final ListingRepository listingRepository;
     public List<Listing> getAllListings(){
         return listingRepository.findAll();
